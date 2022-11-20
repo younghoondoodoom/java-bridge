05c0d3e99efe8171e2f6d5ae75a90da30c0b0ee1
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-20 18:42:27 +0900

    feat(BridgeSpaceStatus): 위, 아래를 나태내는 숫자와 문자로 표시한 열거형 클래스 생성
    구성 요소:
    - 표시 문자
    - 표시 숫자
    구성 메소드:
    - 표시 숫자로 적절한 열거형 찾는 메소드
    - Getter
    - RequiredArgsconstructor

---
+++ src/main/java/bridge/type/BridgeSpaceStatus.java
@@ -0,0 +1,33 @@
+package bridge.type;
+
+import bridge.exception.ErrorCode;
+import java.util.Arrays;
+
+public enum BridgeSpaceStatus {
+    UP("U", 1),
+    DOWN("D", 0);
+    private final String letter;
+    private final int number;
+
+    BridgeSpaceStatus(String letter, int number) {
+        this.letter = letter;
+        this.number = number;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public static BridgeSpaceStatus findByNumber(final int number) {
+        return Arrays.stream(BridgeSpaceStatus.values())
+            .filter(bridgeInformation -> bridgeInformation.number == number)
+            .findAny()
+            .orElseThrow(
+                () -> new IllegalArgumentException(ErrorCode.INVALID_SIGNATURE_NUMBER.getMessage())
+            );
+    }
+}
