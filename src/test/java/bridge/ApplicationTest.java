b946bee7041f41e0714a96da0bfb5e74d918bad1
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-21 22:27:07 +0900

    fix(ApplicationTest): 메소드의 시그니처 변경에 따른 수정

--- src/test/java/bridge/ApplicationTest.java
+++ src/test/java/bridge/ApplicationTest.java
@@ -5,6 +5,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Lists.newArrayList;
 
+import bridge.domain.Bridge;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -17,8 +18,8 @@ class ApplicationTest extends NsTest {
     void 다리_생성_테스트() {
         BridgeNumberGenerator numberGenerator = new TestNumberGenerator(newArrayList(1, 0, 0));
         BridgeMaker bridgeMaker = new BridgeMaker(numberGenerator);
-        List<String> bridge = bridgeMaker.makeBridge(3);
-        assertThat(bridge).containsExactly("U", "D", "D");
+        Bridge bridge = bridgeMaker.makeBridge(3);
+        assertThat(bridge.getSequence()).containsExactly("U", "D", "D");
     }
 
     @Test
