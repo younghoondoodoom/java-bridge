49c538fadde2041dc0f7dd220674d244b434cafd
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-22 01:11:29 +0900

    style(InputContext): 줄 바꿈 처리

--- src/main/java/bridge/util/InputContext.java
+++ src/main/java/bridge/util/InputContext.java
@@ -6,8 +6,7 @@ public class InputContext {
 
     private static final String ERROR_PREFIX = "[ERROR] ";
 
-    public <T> T workWithInputStrategy(InputValidationChain validator,
-        InputStrategy<T> inputStrategy) {
+    public <T> T workWithInputStrategy(InputValidationChain validator, InputStrategy<T> inputStrategy) {
         T input = null;
         while (input == null) {
             try {
