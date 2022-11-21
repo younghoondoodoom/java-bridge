0f9f7447e8facdf9f93dfc74651b0668e85a0ae8
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-21 22:12:02 +0900

    test(BridgeSpaceStatusTest): BridgeSpaceStatus에서 문자로 값 찾기 기능 테스트 코드 작성

--- src/test/java/bridge/type/BridgeSpaceStatusTest.java
+++ src/test/java/bridge/type/BridgeSpaceStatusTest.java
@@ -37,4 +37,34 @@ class BridgeSpaceStatusTest {
         assertThat(up).isEqualTo(BridgeSpaceStatus.UP);
         assertThat(down).isEqualTo(BridgeSpaceStatus.DOWN);
     }
+
+    @Test
+    public void findByLetterInvalidSignatureLetterFailure() {
+        //given
+        String letter = "Q";
+
+        //when
+        //then
+        IllegalArgumentException exception = assertThrows(
+            IllegalArgumentException.class,
+            () -> BridgeSpaceStatus.findByLetter(letter)
+        );
+        assertThat(exception.getMessage()).isEqualTo(
+            ErrorCode.INVALID_SIGNATURE_LETTER.getMessage());
+    }
+
+    @Test
+    public void findByLetterSuccess() {
+        //given
+        String upLetter = BridgeSpaceStatus.UP.getLetter();
+        String downLetter = BridgeSpaceStatus.DOWN.getLetter();
+
+        //when
+        BridgeSpaceStatus up = BridgeSpaceStatus.findByLetter(upLetter);
+        BridgeSpaceStatus down = BridgeSpaceStatus.findByLetter(downLetter);
+
+        //then
+        assertThat(up).isEqualTo(BridgeSpaceStatus.UP);
+        assertThat(down).isEqualTo(BridgeSpaceStatus.DOWN);
+    }
 }
