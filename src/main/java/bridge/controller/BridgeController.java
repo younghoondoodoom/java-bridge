0dc910ac93822ba36b1b55262e74941783ebb073
ChoiYoungHoon <dudgns2865@naver.com>
2022-11-21 23:44:46 +0900

    refactor(BridgeController): 메소드 분리

--- src/main/java/bridge/controller/BridgeController.java
+++ src/main/java/bridge/controller/BridgeController.java
@@ -29,28 +29,44 @@ public class BridgeController {
 
     public void play() {
         OutputView.printStart();
-        final int size = getSize();
-        final Bridge bridge = bridgeMaker.makeBridge(size);
-        Response response = new Response(false, false, 1, 0);
-        while (true) {
-            Request request = new Request(response.getIndex(), response.getAttemptCount(), getMove());
-            response = bridgeGame.move(request, bridge);
-            OutputView.printMap(bridge.getSequence(), response.isPossible(), request.getIndex());
-            if (response.isSuccess()) {
-                break;
-            }
-            if (!response.isPossible()) {
-                if (getGameCommand()) {
-                    response = bridgeGame.retry(request);
-                } else {
-                    break;
-                }
-            }
-        }
+        final Bridge bridge = makeBridge();
+        Response response = crossBridge(bridge);
+        printResult(bridge, response);
+    }
+
+    private void printResult(Bridge bridge, Response response) {
         OutputView.printResult(new PrintResultDto(bridge.getSequence(), response.isSuccess(),
             response.getAttemptCount(), response.getIndex()));
     }
 
+    private Response crossBridge(Bridge bridge) {
+        Response initResponse = new Response(false, false, 1, 0);
+        return getResult(bridge, initResponse);
+    }
+
+    private Response getResult(Bridge bridge, Response response) {
+        while (true) {
+            Request request = new Request(response.getIndex(), response.getAttemptCount(), getMove());
+            response = bridgeGame.move(request, bridge);
+            OutputView.printMap(bridge.getSequence(), response.isPossible(), request.getIndex());
+            if (response.isSuccess() || (!response.isPossible() && !getGameCommand()))
+                break;
+            response = retryCheck(response, request);
+        }
+        return response;
+    }
+
+    private Response retryCheck(Response response, Request request) {
+        if (!response.isPossible()) {
+            response = bridgeGame.retry(request);
+        }
+        return response;
+    }
+
+    private Bridge makeBridge() {
+        final int size = getSize();
+        return bridgeMaker.makeBridge(size);
+    }
 
 
     private Integer getSize() {
